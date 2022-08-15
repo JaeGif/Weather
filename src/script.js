5ee990c285d63526909575727329bc9807258042
Jae Gifford <jae.giff@outlook.com>
2022-08-15 10:21:40 +0800

    define workspace structure, test call OpenWeatherAPI. API Key is free, public and unsecure

---
+++ src/script.js
@@ -0,0 +1,44 @@
+// API Key APPID=a4b3f65b2dbd15c2b33875e013b2dc1b
+// https://api.openweathermap.org/data/2.5/weather?q=London&APPID=a4b3f65b2dbd15c2b33875e013b2dc1b
+// JSON respone Structure:
+/* {
+  coord: { lon: -74.006, lat: 40.7143 },
+  weather: [
+    { id: 801, main: 'Clouds', description: 'few clouds', icon: '02n' }
+  ],
+  base: 'stations',
+  main: {
+    temp: 295.58,
+    feels_like: 295.47,
+    temp_min: 293.57,
+    temp_max: 296.67,
+    pressure: 1017,
+    humidity: 61
+  },
+  visibility: 10000,
+  wind: { speed: 4.92, deg: 360, gust: 8.94 },
+  clouds: { all: 20 },
+  dt: 1660528471,
+  sys: {
+    type: 2,
+    id: 2039034,
+    country: 'US',
+    sunrise: 1660471525,
+    sunset: 1660521372
+  },
+  timezone: -14400,
+  id: 5128581,
+  name: 'New York',
+  cod: 200
+} */
+
+// units = 'imperial / metric
+async function fetchWeather(location = 'New York', units = 'imperial') {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?units=${units}&q=${location}&APPID=a4b3f65b2dbd15c2b33875e013b2dc1b`,
+    { mode: 'cors' }
+  );
+  const dataJSON = await response.json();
+  console.log(dataJSON);
+}
+fetchWeather('London');
