ea28620226443f852352326442417f3753e826ba
Jae Gifford <jae.giff@outlook.com>
2022-08-19 20:10:24 +0800

    bugfix: properly stores all saved locations in capitalized format

--- src/modules/utilities.js
+++ src/modules/utilities.js
@@ -1,6 +1,8 @@
 function capitalize(string) {
-  string.replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
-  return string;
+  let capitalized = string.replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
+    letter.toUpperCase()
+  );
+  return capitalized;
 }
 /**
  * @return {string} date
